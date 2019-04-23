e8d62f6ea1d344976d6cbbe949a76a2780d1ce4b
ZXZCNPOWER <49693076+ZXZCNPOWER@users.noreply.github.com>
2019-04-24 07:41:41 +0800

    Update H_bridge_motor.ino

--- H_bridge_motor/H_bridge_motor.ino
+++ H_bridge_motor/H_bridge_motor.ino
@@ -4,66 +4,110 @@
 
 VL53L0X sensor;
 
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
 
-int relay1=2; //relay pins for drill 1 and 2 is together, 3 and 4 together
+int relay1=2;
 int relay2=3;
 int relay3=4;
 int relay4=5;
+int upbut=6;
+int downbut=7;
+int upbutswitch=0;
+int downswitch=0;
+int limitswitchpin = 8;//limit switch pin
+int dist = 0;
 
-//int upbut=6;
-//int downbut=7;
-//int upbutswitch=0;
-//int downswitch=0; 
-//int initial = 0;
 
-//int State1 = LOW;
-//int State2 = LOW;
-
-//int button1 = 11; //button control first
-//int button2 = 12;
-
-void setup()
-{
+unsigned long startMillis;  //some global variables available anywhere in the program
+unsigned long currentMillis;
+int state=0;
+int i=0;
+int belowcar=0;
+int commandjack=0;
+int limitswitch=0;
+void setup() {
+  
+  pinMode(relay1,OUTPUT);
+  pinMode(relay2,OUTPUT);
+  pinMode(relay3,OUTPUT);
+  pinMode(relay4,OUTPUT);
+  pinMode(limitswitchpin, INPUT);
+  //pinMode(upbut,INPUT);
+  //pinMode(downbut,INPUT);
+  digitalWrite(relay1,HIGH); 
+  digitalWrite(relay2,HIGH); 
+  digitalWrite(relay3,HIGH); 
+  digitalWrite(relay4,HIGH); 
   Serial.begin(9600);
+
   Wire.begin();
 
   sensor.init();
   sensor.setTimeout(500);
 
-  // Start continuous back-to-back mode (take readings as
-  // fast as possible).  To use continuous timed mode
-  // instead, provide a desired inter-measurement period in
-  // ms (e.g. sensor.startContinuous(100)).
   sensor.startContinuous();
 
-//  Serial.println("VLX53LOX test started.");
-  pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
+}
 
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
- 
-  pinMode(relay1,OUTPUT); //relay setup, HIGH=OFF, LOW=ON
-  pinMode(relay2,OUTPUT);
-  pinMode(relay3,OUTPUT);
-  pinMode(relay4,OUTPUT);
-  digitalWrite(relay1,HIGH); 
-  digitalWrite(relay2,HIGH); 
-  digitalWrite(relay3,HIGH); 
-  digitalWrite(relay4,HIGH); 
+void loop() {
 
 
-void loop(){
+  if (state==1){
+    LASERSENSOR();
+    if (i==0){
+      startMillis=millis();
+      i=1;
+      if (commandjack==1){
+        
+        up();
+        if (dist > 100) {
+          belowcar=1;
+        }
+        else{
+          belowcar=0;
+        }
+      }
+      else if (commandjack==2){
+        down();
+      }
+        
+    }
+
+    else {     //i is other number
+      currentMillis=millis();
+      if (currentMillis - startMillis>5000){
+      state=0;
+      i=0;
+      startMillis=0;
+      }
+      else if (commandjack=1){
+        if (belowcar==1){
+          if (dist < 100){
+            belowcar = 0;
+          }
+        }
+        else if (belowcar=0){
+          if (dist>1000){
+            state=0;
+          }
+        }
+
+      }
+      else if (commandjack=2){
+        limitswitch=digitalRead(limitswitchpin);
+        if (limitswitch==HIGH){
+          state=0;
+        }
+      }
+    }
   }
-  
-
+  else{
+    off();
+  }
 
 
 }
+
+
 void up() {
   digitalWrite(relay2,LOW);
   delay(50);
